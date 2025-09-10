50fed44308df531a3b1b53e9a9a6e9fb5fb7df04
diggens42 <frederic.marcel.wahl@gmail.com>
2025-09-11 01:43:04 +0200

    added ft_strtoul() for better errorhandling

---
+++ conversion/ft_strtoul.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtoul.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fwahl <fwahl@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 01:34:49 by fwahl             #+#    #+#             */
+/*   Updated: 2025/09/11 01:42:15 by fwahl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+
+unsigned long ft_strtoul(const char *nptr, char **endptr, int base)
+{
+    const char *s = nptr;
+    unsigned long acc;
+    int c;
+    unsigned long cutoff;
+    int neg = 0;
+    int any = 0;
+
+    c = *s;
+    while (isspace(c))
+        c = *++s;
+
+    if (c == '-')
+    {
+        neg = 1;
+        c = *++s;
+    }
+    else if (c == '+')
+        c = *++s;
+
+    if ((base == 0 || base == 16) && c == '0' && (*(s + 1) == 'x' || *(s + 1) == 'X'))
+    {
+        c = *++s;
+        c = *++s;
+        base = 16;
+    }
+    else if (base == 0)
+    {
+        if (c == '0')
+            base = 8;
+        else
+            base = 10;
+    }
+
+    if (base < 2 || base > 36)
+    {
+        if (endptr)
+            *endptr = (char *)nptr;
+        return 0;
+    }
+
+    cutoff = (unsigned long)-1 / base;
+
+    while (true)
+    {
+        if (isdigit(c))
+            c -= '0';
+        else if (isalpha(c))
+            c -= (isupper(c) ? 'A' - 10 : 'a' - 10);
+        else
+            break;
+
+        if (c >= base)
+            break;
+
+        if (acc > cutoff)
+            any = 1;
+        else
+        {
+            acc *= base;
+            acc += c;
+            any = 1;
+        }
+        c = *++s;
+    }
+
+    if (endptr)
+        *endptr = (char *)(any ? s : nptr);
+
+    return (neg ? -acc : acc);
+}
